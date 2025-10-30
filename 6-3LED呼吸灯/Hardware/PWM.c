552d26dcde699f4b5039451b17ccfe4e9a1cd7a5
navihouse <2165649136@qq.com>
2025-10-30 18:02:25 +0800

    引脚的重映射

--- 6-3LED呼吸灯/Hardware/PWM.c
+++ 6-3LED呼吸灯/Hardware/PWM.c
@@ -6,9 +6,14 @@ void PWM_Init(void)
 	
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);
+	
+	GPIO_PinRemapConfig(GPIO_PartialRemap1_TIM2,ENABLE);
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable,ENABLE);
+	
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
 	
